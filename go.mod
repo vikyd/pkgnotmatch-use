180338fdf2bead77dcc9566edd3d7e931ffedd73
vikyd <zwj_fhys@qq.com>
2019-10-07 21:32:52 +0800

    feat: new import

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/vikyd/pkgnotmatch-use
 
 go 1.13
 
-require github.com/vikyd/pkgnotmatch v1.0.0
+require github.com/vikyd/pkgnotmatch v1.0.1
--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 
 	// can use `github.com/vikyd/pkgnotmatch` directly
 	// but `abc "github.com/vikyd/pkgnotmatch"` will be better
-	xxx "github.com/vikyd/pkgnotmatch"
+	"github.com/vikyd/pkgnotmatch"
 )
 
 func main() {
 	fmt.Println("main")
-	xxx.F01()
+	abc.F01()
 
 }
